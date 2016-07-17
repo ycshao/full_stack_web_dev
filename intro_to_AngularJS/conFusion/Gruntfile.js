d34c075a97e71b1f6aae69661e3615414763b9de
Yuecheng Shao <ycshao0402@gmail.com>
2016-07-17 15:19:10 -0400

    install copy and clean

--- intro_to_AngularJS/conFusion/Gruntfile.js
+++ intro_to_AngularJS/conFusion/Gruntfile.js
@@ -17,17 +17,52 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
-        all: {
-            src: [
-                'Gruntfile.js',
-                'app/scripts/{,*/}*.js'
-            ]
+            copy: {
+                dist: {
+                    cwd: 'app',
+                    src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+                    dest: 'dist',
+                    expand: true
+                }
+            },
+            fonts: {
+                files: [
+                    {
+                        //for bootstrap fonts
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/bootstrap/dist',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    }, 
+                    {
+                        //for font-awesome
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/font-awesome',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    }
+                ]
+            },
+            clean: {
+                build: {
+                    src: [ 'dist/']
+                },
+                all: {
+                    src: [
+                        'Gruntfile.js',
+                        'app/scripts/{,*/}*.js'
+                    ]
+                }
             }
         }
     });
 
     grunt.registerTask('build', [
-        'jshint'
+        'clean',
+        'jshint',
+        'copy'
     ]);
 
     grunt.registerTask('default',['build']);
