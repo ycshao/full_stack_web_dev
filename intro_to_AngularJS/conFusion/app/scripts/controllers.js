931dce27f25aa960aa77810f7c35b59a54a5c8ce
Yuecheng Shao <ycshao0402@gmail.com>
2016-07-25 00:19:19 -0400

    week3 ex4

--- intro_to_AngularJS/conFusion/app/scripts/controllers.js
+++ intro_to_AngularJS/conFusion/app/scripts/controllers.js
@@ -73,10 +73,10 @@ angular.module('confusionApp')
     };
 }])
 
-.controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', 
-function($scope, $routeParams, menuFactory) {
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 
+function($scope, $stateParams, menuFactory) {
 
-    $scope.dish = menuFactory.getDish(parseInt($routeParams.id, 10));
+    $scope.dish = menuFactory.getDish(parseInt($stateParams.id, 10));
     $scope.orderBy = "";
 }])
 
--- intro_to_AngularJS/conFusion/gulpfile.js
+++ intro_to_AngularJS/conFusion/gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-    return gulp.src('app/*.html')
+    return gulp.src('app/**/*.html')
     .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
