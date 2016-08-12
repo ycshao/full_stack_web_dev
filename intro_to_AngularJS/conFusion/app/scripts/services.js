67d9290578d9ccc6e5dc74e3e2fab0510181d4b1
Yuecheng Shao <ycshao0402@gmail.com>
2016-08-12 00:06:09 -0400

    week4 ex4

--- intro_to_AngularJS/conFusion/app/scripts/services.js
+++ intro_to_AngularJS/conFusion/app/scripts/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('confusionApp')
-.constant("baseURL","http://localhost:3000/")
+.constant("baseURL", "http://localhost:3000/")
 .service('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
 
     var promotions = [
