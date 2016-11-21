04a3369d2ec77503539f28810a495826bd94eb77
Yuecheng Shao <ycshao0402@gmail.com>
2016-11-20 23:57:47 -0500

    week4 ex3 Oauth

--- server_side_development/rest-server-passport/config.js
+++ server_side_development/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1613562398948300',
+        clientSecret: '0589323b62603d5caa0eb5adf8dd36b7',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
