ebc84df422a299917a3d0e0d5fd7935cc4e2dc3e
Yuecheng Shao <ycshao0402@gmail.com>
2016-11-20 22:40:04 -0500

    week4 ex2 ssl

--- server_side_development/rest-server-passport/app.js
+++ server_side_development/rest-server-passport/app.js
@@ -13,9 +13,9 @@ var config = require('./config');
 mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-  console.log("Connected correctly to server");
+db.once('open', function() {
+    // we're connected!
+    console.log("Connected correctly to server");
 });
 
 var routes = require('./routes/index');
@@ -53,33 +53,43 @@ app.use('/promotions', promoRouter);
 app.use('/leadership', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
+});
+
+// Secure traffic only
+app.all('*', function(req, res, next) {
+    console.log('req start: ', req.secure, req.hostname, req.url, app.get('port'));
+    if (req.secure) {
+        return next();
+    };
+
+    res.redirect('https://' + req.hostname + ':' + app.get('secPort') + req.url);
 });
 
 module.exports = app;
